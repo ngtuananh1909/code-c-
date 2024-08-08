63ecd8b64a416f0544f488481528034fd5abb39b
tuananh <nta1909k7@gmail.com>
2024-08-08 17:22:55 +0700

    new commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define test "main"
+#define endl '\n'
+#define PRO_NHI_PHONG_TIN int main()
+#define sync ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+
+void input()
+{
+    sync;
+    freopen(test".inp","r",stdin);
+    freopen(test".out","w",stdout);
+}
+
+PRO_NHI_PHONG_TIN
+{
+    input();
+    int n=5; cout<<n;
+    return 0;
+}
