8f6ef409a6c62de88369c1b2ee3d68fc2766f687
tuananh <nta1909k7@gmail.com>
2024-08-10 20:45:56 +0700

    xaudoixungdainhat

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define sync ios_base::sync_with_stdio(false); cin.tie(0);cout.tie(0);
-#define text "coprime"
+#define text "main"
 using namespace std;
 
 void input()
@@ -10,47 +10,68 @@ void input()
     freopen(text".out","w",stdout);
 }
 
-bool uocchung(int a,int b)
+bool check(string& s)
 {
-    while (a!=0)
+    int i=0,f=s.size()-1;
+    while (i<f)
     {
-        int tmp=b%a;
-        b=a;
-        a=tmp;
+        if (s[i]!=s[f])
+        {
+            return false;
+        }
+        i++;
+        f--;
     }
-    return b==1;
+    return true;
+}
+
+int longestPalindromeSubstring(const string& s) {
+    int n = s.length();
+    if (n == 0) return 0;
+
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int maxLength = 1;
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = true;
+    }
+
+    int start = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = true;
+            start = i;
+            maxLength = 2;
+        }
+    }
+    for (int length = 3; length <= n; length++) {
+        for (int i = 0; i < n - length + 1; i++) {
+            int j = i + length - 1;
+            if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                dp[i][j] = true;
+
+                if (length > maxLength) {
+                    start = i;
+                    maxLength = length;
+                }
+            }
+        }
+    }
+
+    return maxLength;
 }
 
 int main()
 {
     input();
-    int n; cin>>n;
-    vector<int> a(n+1);
-    vector<bool> check(n+1,false);
-    for (int i=1;i<=n;i++)
-    {
-        cin>>a[i];
-        if (i!=1)
-        {
-            check[i-1]=uocchung(a[i],a[i-1]);
-        }
-    }
-    int dem=0,res=0;
-    for (int i=1;i<=n;i++)
-    {
-        if (check[i]) dem++;
-        else if (!check[i]) {
-            dem++;
-            if (dem==1)
-            {
-                dem=0;
-                continue;
-            }
-            res=max(dem,res);
-            dem=0;
-        }
-    }
-    cout<<max(res,dem);
+    string s; cin>>s;
+    // for (int i=0;i<s.size()-1;i++)
+    // {
+    //     for (int f=i+1;f<s.size();f++)
+    //     {
+
+    //     }
+    // }
+    cout<<longestPalindromeSubstring(s);
     return 0;
 }
 
