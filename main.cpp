c411b1b5bcb4beda85a687ec5f361c413ad9b2fe
nta <nta1909k7@gmail.com>
2024-10-09 20:50:28 +0700

    sussss

--- main.cpp
+++ main.cpp
@@ -10,10 +10,46 @@ void input()
     freopen(text".out","w",stdout);
 }
 
+bool pali(int& n)
+{
+    string s=to_string(n);
+    string test=string(s.rbegin(),s.rend());
+    return test==s;
+}
+
+set<int> count(int& n)
+{
+    set<int> save;
+    while (n%2==0)
+    {
+        save.insert(2);
+        n/=2;
+    }
+    for (int i=3;i*i<=n;i+=2)
+    {
+        while(n%i==0)
+        {
+            save.insert(i);
+            n/=i;
+        }
+    }
+    if (n>2) save.insert(n);
+    return save;
+}
+
 int main()
 {
     input();
-    
+    int a,b,res=0; cin>>a>>b;
+    for (int i=66;i<=b;i++)
+    {
+        if (pali(i))
+        {
+            set<int> tmp=count(i);
+            if (tmp.size()>=3) res++;
+        }
+    }
+    cout<<res;
     return 0;
 }
 
